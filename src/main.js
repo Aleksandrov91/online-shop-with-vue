cde179ceca36b63299217274d322bdf2be060afa
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-01-28 00:40:09 +0200

    Added bootstrap

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import App from './App';
 import router from './router';
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 /* eslint-disable no-new */
 new Vue({
