a9d4c18a5c89c104b1715faac2205d47f2da34e2
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-01-31 23:11:53 +0200

    Renamed admin page

---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="admin-new mt-5">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                    <ul class="list-group">
+                        <li class="list-group-item"><router-link to="/admin">View Products</router-link></li>
+                        <li class="list-group-item"><router-link to="/admin/new">New Product</router-link></li>
+                    </ul>
+                </div>
+
+                <router-view class="mt-sm-3 offset-2"></router-view>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
