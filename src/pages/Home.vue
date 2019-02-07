12effb7a8d1a03de3c22f2967fa61f0210a59f22
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-02-07 23:05:22 +0200

    Use Product list component to display all products

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,21 @@
 <template>
-    <div>
-        <div class="title">
-            <h1>{{msg}}</h1>
-        </div>
+  <div>
+    <div class="title">
+      <h1>
+        <i class="fa fa-braille"></i> In Stock
+      </h1>
     </div>
+    <product-list></product-list>
+  </div>
 </template>
 
 <script>
+import ProductList from '../components/product/ProductList.vue';
+
 export default {
   name: 'home',
-  data() {
-    return {
-      msg: 'Welcome to our online shop.',
-    };
+  components: {
+    'product-list': ProductList,
   },
 };
 </script>
