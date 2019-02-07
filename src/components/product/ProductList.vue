b013ffc4be8f01d26f3b08d3af51125ee1310706
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-02-07 23:04:35 +0200

    Added product components

---
+++ src/components/product/ProductList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <template v-for="product in products">
+      <product-item :product="product" :key="product.id"></product-item>
+    </template>
+  </div>
+</template>
+
+<script>
+import ProductItem from "./ProductItem.vue";
+export default {
+  name: "product-item",
+  created() {
+    if (this.products.length === 0) {
+      console.log(this.$store);
+      this.$store.dispatch("allProducts");
+    }
+  },
+  computed: {
+    products() {
+      return this.$store.getters.allProducts;
+    }
+  },
+  components: {
+    "product-item": ProductItem
+  }
+};
+</script>
