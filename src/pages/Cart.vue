7eec454f42c91d3368b7f75209c640adff80b9f7
Aleksandrov91 <a.aleksandrov91@gmail.com>
2019-02-09 11:38:19 +0200

    Update Cart view

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,8 +1,26 @@
 <template>
-<h1>Welcome to Cart page.</h1>
+  <div v-if="cart.length > 0">
+    <h1>Your Cart</h1>
+    <template v-for="product in products">
+      <product-details :key="product._id" :product="product"></product-details>
+    </template>
+  </div>
+  <div v-else>
+    <h1>Your Cart is empty.</h1>
+  </div>
 </template>
 
 <script>
+import ProductDetails from "../components/product/ProductDetails";
+
 export default {
+  data() {
+    return {
+      cart: this.$store.state.cart
+    };
+  },
+  components: {
+    "product-details": ProductDetails
+  }
 };
 </script>
